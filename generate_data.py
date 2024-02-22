8ed168d4efea3ad6b1e7e13b42dc1458a561cb8e
TristanSaidi <83875235+TristanSaidi@users.noreply.github.com>
2024-02-22 18:24:39 -0500

    allow option to avoid manifold edges in subgraph sampling (#6)

--- generate_data.py
+++ generate_data.py
@@ -91,7 +91,7 @@ def create_torus_dataset(inner_radius, outer_radius, N, k=10, features_max_k=250
     return torus_data, X
 
 
-def create_euclidean_dataset(N, d, rad, k=10, features_max_k=2500, device='cpu', path=None, scale_labels=False):
+def create_euclidean_dataset(N, d, rad, k=10, features_max_k=2500, device='cpu', path=None, avoid_edge=False):
     print(f'Creating euclidean dataset with {N} nodes, dimension {d}, and radius {rad}')
     # Sample from manifold
     X = manifold.Euclidean.sample(N, d, rad)
@@ -105,18 +105,49 @@ def create_euclidean_dataset(N, d, rad, k=10, features_max_k=2500, device='cpu',
     # Create node labels (scalar curvature in this case)
     node_labels = np.zeros((N, 1))
     node_labels = torch.tensor(node_labels).to(device)
+
+    # avoid edge of euclidean disk in training and validation --> check norm of nodes
+    if avoid_edge:
+        norms = []
+        for row in range(X.shape[0]):
+            norm = np.linalg.norm(X[row], ord=2)
+            norms.append(norm)
+        norms = np.stack(norms)
+        near_center_indices = np.argwhere(norms <= rad * 0.8)[:,0]
+        X = X[near_center_indices]
+        node_features = node_features[near_center_indices]
+        node_labels = node_labels[near_center_indices]
+        # filter edges
+        valid_edge_indices = [] # indices in the original edge list
+        valid_edges = [] # edge list given new vertex numbering
+        for row_idx in range(edge_list.shape[0]):
+            edge = edge_list[row_idx]
+            v1, v2 = edge[0].item(), edge[1].item()
+            v1_valid = v1 in near_center_indices
+            v2_valid = v2 in near_center_indices
+            if v1_valid and v2_valid:
+                valid_edge_indices.append(row_idx)
+                v1_new = np.argwhere(near_center_indices == v1)[0]
+                v2_new = np.argwhere(near_center_indices == v2)[0]
+                new_edge = np.array([v1_new, v2_new])[:, 0]
+                valid_edges.append(new_edge)
+        edge_list = torch.tensor(np.stack(valid_edges)).to(device)
+
+        valid_edge_indices = np.stack(valid_edge_indices)
+        edge_attrs = edge_attrs[valid_edge_indices] 
+    else:
+        assert edge_list.shape == (N*k, 2)
+        assert edge_attrs.shape == (N*k,1)
+        assert node_labels.shape == (N,1)
+    
     euclidean_data = Data(x=node_features, edge_index=edge_list, edge_attr=edge_attrs, y=node_labels)
 
-    assert edge_list.shape == (N*k, 2)
-    assert edge_attrs.shape == (N*k,1)
-    assert node_labels.shape == (N,1)
-    
     if path is not None:
         torch.save(euclidean_data, path)
     return euclidean_data, X
 
 
-def create_poincare_dataset(N, K, k, Rh, features_max_k=2500, device='cpu', path=None, scale_labels=False):
+def create_poincare_dataset(N, K, k, Rh, features_max_k=2500, device='cpu', path=None, scale_labels=False, avoid_edge=False):
     print(f'Creating poincare dataset with {N} nodes, curvature {K}, and radius {Rh}')
     X = manifold.PoincareDisk.sample(N=N, K=K, Rh=Rh)
     # data isn't embedded in euclidean space --> need to compute hyperbolic distances for kNN
@@ -143,17 +174,46 @@ def create_poincare_dataset(N, K, k, Rh, features_max_k=2500, device='cpu', path
         scaling_factors = np.expand_dims(np.array(scaling_factors), -1)
         node_labels = node_labels / scaling_factors
     node_labels = torch.tensor(node_labels).to(device)
+
+    # avoid edge of poincare disk in training and validation --> check norm of nodes
+    if avoid_edge:
+        norms = []
+        for row in range(X.shape[0]):
+            norm = manifold.PoincareDisk.norm(X[row], K)
+            norms.append(norm)
+        norms = np.stack(norms)
+        near_center_indices = np.argwhere(norms <= Rh*0.8)[:,0]
+        X = X[near_center_indices]
+        node_features = node_features[near_center_indices]
+        node_labels = node_labels[near_center_indices]
+        # filter edges
+        valid_edge_indices = [] # indices in the original edge list
+        valid_edges = [] # edge list given new vertex numbering
+        for row_idx in range(edge_list.shape[0]):
+            edge = edge_list[row_idx]
+            v1, v2 = edge[0].item(), edge[1].item()
+            v1_valid = v1 in near_center_indices
+            v2_valid = v2 in near_center_indices
+            if v1_valid and v2_valid:
+                valid_edge_indices.append(row_idx)
+                v1_new = np.argwhere(near_center_indices == v1)[0]
+                v2_new = np.argwhere(near_center_indices == v2)[0]
+                new_edge = np.array([v1_new, v2_new])[:, 0]
+                valid_edges.append(new_edge)
+        valid_edge_indices = np.stack(valid_edge_indices)
+        edge_list = torch.tensor(np.stack(valid_edges)).to(device)
+        edge_attrs = edge_attrs[valid_edge_indices] 
+    else:
+        assert edge_list.shape == (N*k, 2)
+        assert edge_attrs.shape == (N*k,1)
+        assert node_labels.shape == (N,1)
+
     poincare_data = Data(x=node_features, edge_index=edge_list, edge_attr=edge_attrs, y=node_labels)
-
-    assert edge_list.shape == (N*k, 2)
-    assert edge_attrs.shape == (N*k,1)
-    assert node_labels.shape == (N,1)
-
     if path is not None:
         torch.save(poincare_data, path)
     return poincare_data, X
 
-def create_hyperbolic_dataset(N, k=10, features_max_k=2500, device='cpu', path=None, scale_labels=False):
+def create_hyperbolic_dataset(N, k=10, features_max_k=2500, device='cpu', path=None, scale_labels=False, avoid_edge=False):
     print(f'Creating hyperbolic dataset with {N} nodes')
     X = manifold.Hyperboloid.sample(N)
     adjacency_mat = neighbors.kneighbors_graph(X, n_neighbors=k, mode='distance', include_self=False)
@@ -173,12 +233,36 @@ def create_hyperbolic_dataset(N, k=10, features_max_k=2500, device='cpu', path=N
         scaling_factors = np.expand_dims(np.array(scaling_factors), -1)
         node_labels = node_labels / scaling_factors
     node_labels = torch.tensor(node_labels).to(device)
+
+    # avoid edge of hyperboloid in training and validation --> check z of nodes
+    if avoid_edge:
+        near_center_indices = np.argwhere(np.abs(X[:, 2]) <= 0.8)[:,0]
+        X = X[near_center_indices]
+        node_features = node_features[near_center_indices]
+        node_labels = node_labels[near_center_indices]
+        # filter edges
+        valid_edge_indices = [] # indices in the original edge list
+        valid_edges = [] # edge list given new vertex numbering
+        for row_idx in range(edge_list.shape[0]):
+            edge = edge_list[row_idx]
+            v1, v2 = edge[0].item(), edge[1].item()
+            v1_valid = v1 in near_center_indices
+            v2_valid = v2 in near_center_indices
+            if v1_valid and v2_valid:
+                valid_edge_indices.append(row_idx)
+                v1_new = np.argwhere(near_center_indices == v1)[0]
+                v2_new = np.argwhere(near_center_indices == v2)[0]
+                new_edge = np.array([v1_new, v2_new])[:, 0]
+                valid_edges.append(new_edge)
+        valid_edge_indices = np.stack(valid_edge_indices)
+        edge_list = torch.tensor(np.stack(valid_edges)).to(device)
+        edge_attrs = edge_attrs[valid_edge_indices] 
+    else:
+        assert edge_list.shape == (N*k, 2)
+        assert edge_attrs.shape == (N*k,1)
+        assert node_labels.shape == (N,1)
+
     hyperbolic_data = Data(x=node_features, edge_index=edge_list, edge_attr=edge_attrs, y=node_labels)
-
-    assert edge_list.shape == (N*k, 2)
-    assert edge_attrs.shape == (N*k,1)
-    assert node_labels.shape == (N,1)
-
     if path is not None:
         torch.save(hyperbolic_data, path)
     return hyperbolic_data, X
@@ -242,7 +326,7 @@ def main():
     # Create euclidean data
     d = 2
     rad = 2
-    create_euclidean_dataset(N, d, rad, k, path=os.path.join(output_dir, f'euclidean_dim_{d}_rad_{rad}_nodes_{N}_k_{k}.pt'), features_max_k=features_max_k, scale_labels=scale_labels)
+    create_euclidean_dataset(N, d, rad, k, path=os.path.join(output_dir, f'euclidean_dim_{d}_rad_{rad}_nodes_{N}_k_{k}.pt'), features_max_k=features_max_k)
     # Create poincare data
     Rh = 1
     Ks = [-2, -1]
