25ce93905b65b740589f226961efc62541ed1030
Tristan Saidi <tls2160@columbia.edu>
2024-02-23 13:54:58 -0500

    add option for one-hot indegree features

--- src/datasets/dataset.py
+++ src/datasets/dataset.py
@@ -6,13 +6,19 @@ from torch_geometric.data import Data
 import os
 from tqdm import tqdm
 import numpy as np
+from torch_geometric.utils import degree
+from torch_geometric.transforms import OneHotDegree
+
 
 class ManifoldGraphDataset(Dataset):
-    def __init__(self, full_graphs, subgraph_k, subsample_pctg=0.5):
+    def __init__(self, full_graphs, subgraph_k, degree_features, subsample_pctg=0.1):
         super(ManifoldGraphDataset, self).__init__()
         self.subgraph_k = subgraph_k
         self.subsample_pctg = subsample_pctg
         self.full_graphs = [full_graphs] if not isinstance(full_graphs, list) else full_graphs
+        self.degree_features = degree_features
+        if self.degree_features:
+            self.transform = OneHotDegree(max_degree=10)
         self.prepare_data()
 
     def prepare_data(self):
@@ -25,6 +31,8 @@ class ManifoldGraphDataset(Dataset):
                 if len(edge_indices.shape) == 2:
                     edge_indices = edge_indices.squeeze(1)
                 subgraph = Data(x=full_graph.x[subgraph_nodes], edge_index=subgraph_edges, edge_attr=full_graph.edge_attr[edge_indices].clone(), y=full_graph.y[idx])
+                if self.degree_features:
+                    subgraph = self.transform(subgraph)
                 data_list.append(subgraph)
         self.data = data_list
 
